60101e75e2e67a9fab121be87209d59154e17065
Kei-Luna <touhousetsuna@gmail.com>
2026-05-25 13:25:35 +0900

    DreamCard can Enter

--- Common/Data/GameData.cs
+++ Common/Data/GameData.cs
@@ -58,6 +58,7 @@ public static class GameData
     public static Dictionary<ulong, MonsterCardExcel> MonsterCardData { get; private set; } = [];
     public static Dictionary<uint, FishingFoodExcel> FishingFoodData { get; private set; } = [];
     public static Dictionary<uint, VirCaptureTowerExcel> VirCaptureTowerData { get; private set; } = [];
+    public static Dictionary<uint, DreamCardActivityExcel> DreamCardActivityData { get; private set; } = [];
 }
 
 public static class GameResourceTemplateId
